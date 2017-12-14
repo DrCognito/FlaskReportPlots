da31826fb91af4bb0eaf3ce5d6b9b14382aca231
M Chadwick <dr.m.chadwick@gmail.com>
2017-12-14 15:22:52 +0000

    Added support for flask migrations.

--- ReplayRetriever/__init__.py
+++ ReplayRetriever/__init__.py
@@ -5,9 +5,12 @@ Provides the information required to filter and download replays.
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object('config')
+
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 from ReplayRetriever import models
